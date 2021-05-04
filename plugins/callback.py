abcfca078dde93ac62997efe5b4d96ddfa84feac
Khaled <31672356+joaokhaled@users.noreply.github.com>
2021-05-04 20:47:56 -0300

    Add files via upload

---
+++ plugins/callback.py
@@ -0,0 +1,81 @@
+from pyrogram import Client, filters
+from pyrogram.types import (
+        InlineKeyboardMarkup,
+        InlineKeyboardButton)
+
+from strin import donate, hlp
+
+
+
+@Client.on_callback_query(filters.regex(r"donate"))
+async def etanod(c, m):
+    keyboard = InlineKeyboardMarkup(
+            inline_keyboard=[
+                [
+                    InlineKeyboardButton(
+                            text="💵 PicPay", url="#"
+                    ),
+                  
+                    InlineKeyboardButton(
+                            text="💵 PayPal", url="#"
+                    )
+                ],
+                [
+                    InlineKeyboardButton(
+                            text="💵 Pix", callback_data="gpix"
+                    )
+                ],
+                [
+                    InlineKeyboardButton(
+                            text="◀️ Voltar", callback_data="help"
+                    )
+                ]
+            ]
+        )
+    await m.edit_message_text(donate, reply_markup=keyboard)
+
+
+
+@Client.on_callback_query(filters.regex(r"help"))
+async def pleh(c, m):
+    keyvoard = InlineKeyboardMarkup(
+            inline_keyboard=[
+                [
+                    InlineKeyboardButton(
+                            text="💵 Colabore", callback_data="donate"
+                    )
+                ],
+                [
+                    InlineKeyboardButton(
+                            text="💻 Meu desenvolvedor", url="t.me/khaledsecond"
+                    )
+                ],
+                [
+                    InlineKeyboardButton(
+                            text="🔎 Consulte inline", switch_inline_query_current_chat=""
+                    )
+                ]
+            ]
+        )
+    await m.edit_message_text(hlp, reply_markup=keyvoard)
+
+
+
+@Client.on_callback_query(filters.regex(r"gpix"))
+async def xipg(c, m):
+    keybaard = InlineKeyboardMarkup(
+            inline_keyboard=[
+                [
+                    InlineKeyboardButton(
+                            text="🗑 Apagar mensagem", callback_data="delete"
+                    )
+                ]
+            ]
+        )
+    await c.send_photo(m.message.chat.id, photo="#", caption="#", reply_markup=keybaard)
+    
+
+
+@Client.on_callback_query(filters.regex(r"delete"))
+async def eteled(c, m):
+    await c.delete_messages(m.message.chat.id, m.message.message_id)
---
+++ plugins/listen.py
@@ -0,0 +1,32 @@
+from strin import template
+from configs import PROXY
+from datetime import datetime as l
+from pyrogram import Client, filters
+
+import re
+import httpx
+
+
+
+pattern = "((^| |\n)([a-z]{3}-?[0-9][a-z0-9][0-9]{2})( |\n|$))"
+@Client.on_message(filters.regex(pattern))
+async def listen(c, m):
+    p = re.compile(pattern)
+    q = p.search(m.text)
+    r = q.group(1)
+    s = re.sub("[^a-zA-Z0-9]", "", r).upper()
+    with httpx.Client(proxies=PROXY) as cli:
+        v = cli.get(f"http://api.masterplaca.devplank.com/v51/placa/{s}/json").json()
+    
+    if v["codigoRetorno"] == 98:
+        await m.reply_text("⚠️ <b>{}.</b>".format(v["mensagemRetorno"]), quote=True, parse_mode = "HTML")
+			
+    else:
+        await m.reply_text(template.format(l.now().strftime("%d/%m/%Y ás %H:%M:%S"), s, 
+	            			 	         v["chassi"],
+	            		     	         v["modelo"], 
+	            			 			 v["cor"].upper(), 
+	            			 		     v["ano"], 
+	            			 			 v["municipio"].upper(), 
+	            			 			 v["uf"], 
+	            			 			 v["situacao"]), quote=True, parse_mode = "HTML")
