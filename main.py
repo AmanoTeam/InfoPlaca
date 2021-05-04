7e64b1120aeba87d12a8990d2d64c1da974a3f9b
Khaled <31672356+joaokhaled@users.noreply.github.com>
2021-05-04 20:19:56 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+from pyrogram import Client, idle
+from configs import API_ID, API_HASH, TOKEN
+
+import asyncio
+
+
+
+async def main():
+	await client.start()
+	await idle()
+
+
+client = Client("bot", 
+                API_ID, 
+                API_HASH, 
+                bot_token = TOKEN, 
+                plugins = dict(root="plugins"))
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
