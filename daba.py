527cb3c79ee49141deaf6fdcc3059b24b626435e
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-05-05 14:37:38 +0300

    Update daba.py

--- daba.py
+++ daba.py
@@ -4,15 +4,20 @@ db = sqlite3.connect("placabot.db")
 cursor = db.cursor()
 
 cursor.execute(
-        """CREATE TABLE IF NOT EXISTS users (user_id INTEGER PRIMARY KEY, 
+    """CREATE TABLE IF NOT EXISTS users (user_id INTEGER PRIMARY KEY, 
                                              historic,
                                              is_premium INTEGER)"""
-        
 )
 
-cursor.execute(
-        """CREATE TABLE IF NOT EXISTS groups (chat_id INTEGER PRIMARY KEY)"""
-        
-)
+cursor.execute("""CREATE TABLE IF NOT EXISTS groups (chat_id INTEGER PRIMARY KEY)""")
+
+
+def add_user(user_id):
+    try:
+        cursor.execute("""INSERT INTO users(user_id) VALUES (?)""", (user_id,))
+    except sqlite3.IntegrityError:
+        pass
+    db.commit()
+
 
 db.commit()
