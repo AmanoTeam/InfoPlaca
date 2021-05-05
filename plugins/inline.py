e78c1eb69f0245020ea137e8b8b99c38e6d242cf
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-05-05 14:36:53 +0300

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -1,9 +1,7 @@
 from pyrogram import Client, filters
-from pyrogram.types import (
-    InlineQueryResultArticle,
-    InputTextMessageContent)
+from pyrogram.types import InlineQueryResultArticle, InputTextMessageContent
 
-from strin import template
+from string import template
 from configs import PROXY
 from datetime import datetime as l
 
@@ -11,10 +9,9 @@ import re
 import httpx
 
 
-
 @Client.on_inline_query()
 async def inline(c, m):
-# PADRAO CONVENCIONAL	
+    # PADRAO CONVENCIONAL
     regex_c = "(( |\n|^)[A-Za-z]{3}(( - )| |-)?[0-9]{4}( |\n|$))"
     if re.search(regex_c, m.query):
         p = re.compile(regex_c)
@@ -23,39 +20,54 @@ async def inline(c, m):
         f = re.sub("[^a-zA-Z0-9]", "", placa)
         g = re.sub(r"([a-zA-Z]+)[- ]?(\d+)", r"\1-\2", f).upper()
         with httpx.Client(proxies=PROXY) as cli:
-            h = cli.get(f"http://api.masterplaca.devplank.com/v51/placa/{f}/json").json()
-	
+            h = cli.get(
+                f"http://api.masterplaca.devplank.com/v51/placa/{f}/json"
+            ).json()
+
         if h["codigoRetorno"] == 98:
             await m.answer(
                 [
-                        InlineQueryResultArticle(
-	                        title = f"⚠️ {h['mensagemRetorno']}",
-	                        thumb_url = "https://piics.ml/i/015.png",
-	                        input_message_content = InputTextMessageContent(
-	                        message_text = f"⚠️ <b>{h['mensagemRetorno']}.</b>", parse_mode = "HTML"))
-                ])
-			
+                    InlineQueryResultArticle(
+                        title=f"⚠️ {h['mensagemRetorno']}",
+                        thumb_url="https://piics.ml/i/015.png",
+                        input_message_content=InputTextMessageContent(
+                            message_text=f"⚠️ <b>{h['mensagemRetorno']}.</b>",
+                            parse_mode="HTML",
+                        ),
+                    )
+                ]
+            )
+
         else:
             await m.answer(
                 [
-                        InlineQueryResultArticle(
-	                        title = "Resultado para: " + g, 
-	                        thumb_url = "https://piics.ml/i/015.png",
-	                        input_message_content = InputTextMessageContent(
-	                        str(template.format(l.now().strftime("%d/%m/%Y às %H:%M:%S"), g, 
-	            			                	h["chassi"],
-	            				            	h["modelo"], 	            										  
-	            				            	h["cor"].upper(), 
-	            				            	h["ano"], 
-	            				            	h["municipio"].upper(), 
-	            				            	h["uf"], 
-	            				            	h["situacao"])), parse_mode = "HTML"))
-                ])
+                    InlineQueryResultArticle(
+                        title="Resultado para: " + g,
+                        thumb_url="https://piics.ml/i/015.png",
+                        input_message_content=InputTextMessageContent(
+                            str(
+                                template.format(
+                                    l.now().strftime("%d/%m/%Y às %H:%M:%S"),
+                                    g,
+                                    h["chassi"],
+                                    h["modelo"],
+                                    h["cor"].upper(),
+                                    h["ano"],
+                                    h["municipio"].upper(),
+                                    h["uf"],
+                                    h["situacao"],
+                                )
+                            ),
+                            parse_mode="HTML",
+                        ),
+                    )
+                ]
+            )
 
     else:
         pass
 
-# PADRAO MERCOSUL	
+    # PADRAO MERCOSUL
     regex_m = r"((^| |\n)([a-zA-Z]{3}[0-9]{1}[a-zA-Z][0-9]{2})( |\n|$))"
     if re.search(regex_m, m.query):
         pr = re.compile(regex_m)
@@ -64,34 +76,46 @@ async def inline(c, m):
         p = re.sub("[^a-zA-Z0-9]", "", plac)
         with httpx.Client(proxies=PROXY) as cl:
             q = cl.get(f"http://api.masterplaca.devplank.com/v51/placa/{p}/json").json()
-            
-		
+
         if q["codigoRetorno"] == 98:
             await m.answer(
-                 [
-                        InlineQueryResultArticle(
-	                        title = f"⚠️ {q['mensagemRetorno']}", 
-	                        thumb_url = "https://piics.ml/i/015.png",
-	                        input_message_content = InputTextMessageContent(
-	                        message_text = f"⚠️ <b>{q['mensagemRetorno']}.</b>", parse_mode = "HTML"))
-                 ])
+                [
+                    InlineQueryResultArticle(
+                        title=f"⚠️ {q['mensagemRetorno']}",
+                        thumb_url="https://piics.ml/i/015.png",
+                        input_message_content=InputTextMessageContent(
+                            message_text=f"⚠️ <b>{q['mensagemRetorno']}.</b>",
+                            parse_mode="HTML",
+                        ),
+                    )
+                ]
+            )
 
         else:
             await m.answer(
-                 [
-                        InlineQueryResultArticle(
-	                        title = "Resultado para: " + p.upper(), 
-	                        thumb_url = "https://piics.ml/i/015.png",
-	                        input_message_content = InputTextMessageContent(
-	                        str(template.format(l.now().strftime("%d/%m/%Y às %H:%M:%S"), p.upper(), 
-	            			 				   q["chassi"],
-	            		     				   q["modelo"], 
-	            			 				   q["cor"].upper(), 
-	            			 				   q["ano"], 
-	            			 				   q["municipio"].upper(), 
-	            			 				   q["uf"], 
-	            			 				   q["situacao"])), parse_mode = "HTML"))
-                 ])
+                [
+                    InlineQueryResultArticle(
+                        title="Resultado para: " + p.upper(),
+                        thumb_url="https://piics.ml/i/015.png",
+                        input_message_content=InputTextMessageContent(
+                            str(
+                                template.format(
+                                    l.now().strftime("%d/%m/%Y às %H:%M:%S"),
+                                    p.upper(),
+                                    q["chassi"],
+                                    q["modelo"],
+                                    q["cor"].upper(),
+                                    q["ano"],
+                                    q["municipio"].upper(),
+                                    q["uf"],
+                                    q["situacao"],
+                                )
+                            ),
+                            parse_mode="HTML",
+                        ),
+                    )
+                ]
+            )
 
-    else: 
+    else:
         pass
